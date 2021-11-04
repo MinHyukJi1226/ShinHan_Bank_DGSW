d5e4341017cacc0bdcab136865a3dffa07514f83
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-11-04 14:56:01 +0900

    Login 성공 시 MainActivity 이동

--- ShinHanBank/app/src/main/java/com/hmj/shinhanbank/fragment/SignInWithIdPwFragment.kt
+++ ShinHanBank/app/src/main/java/com/hmj/shinhanbank/fragment/SignInWithIdPwFragment.kt
@@ -1,6 +1,8 @@
 package com.hmj.shinhanbank.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.hmj.shinhanbank.R
+import com.hmj.shinhanbank.activity.MainActivity
 import com.hmj.shinhanbank.databinding.FragmentSignInBinding
 import com.hmj.shinhanbank.databinding.FragmentSignInWithIdPwBinding
 import com.hmj.shinhanbank.network.dto.Request.LoginRequest
@@ -47,7 +50,9 @@ class SignInWithIdPwFragment : Fragment() {
                 } else {
                     PreferenceUtils.token = it.msg
                     Toast.makeText(context, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
-                    findNavController().navigate(SignInWithIdPwFragmentDirections.actionSignInWithIdPwFragmentToMainFragment())
+                    Log.d("Login : ", "로그인 성공")
+                    val intent = Intent(requireContext(), MainActivity::class.java)
+                    startActivity(intent)
                 }
             } else {
                 Toast.makeText(requireContext(), "서버 통신에 실패하였습니다.", Toast.LENGTH_SHORT).show()
