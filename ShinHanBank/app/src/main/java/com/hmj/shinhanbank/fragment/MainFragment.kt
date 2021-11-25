cd283eb4a58bacc4cf6f819165744d006ab24c9d
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-11-25 19:59:55 +0900

    [ADD] MainFragment

--- ShinHanBank/app/src/main/java/com/hmj/shinhanbank/fragment/MainFragment.kt
+++ ShinHanBank/app/src/main/java/com/hmj/shinhanbank/fragment/MainFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hmj.shinhanbank.R
+import com.hmj.shinhanbank.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createLayout.setOnClickListener {
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToInformationCheckFragment(1))
+        }
     }
 
 }
